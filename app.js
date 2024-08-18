33858d8a15cf690d1478839cefbedd2585b90bf4
Mr Akhtar <mogulking@gmail.com>
2024-08-18 15:57:54 +0100

    display cart items in cart dropdown complete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+const productsList = document.getElementById("product-list");
 const cartDropdown = document.getElementById("cart");
 const cartIcon = document.querySelector(".cart-icon");
 const addToCartButton = document.getElementById("add-to-cart");
@@ -8,6 +8,9 @@ const cart  = new Cart();
 function toggleCartOpen() {
     if (cartDropdown.classList.contains('hide-cart')) {
         cartDropdown.classList.remove('hide-cart');
+        if (!cart.getCartItems().length) {
+            cartDropdown.innerHTML = "<p>Cart is empty</p>";
+        }
     } else {
         cartDropdown.classList.add('hide-cart');
     }
@@ -15,14 +18,67 @@ function toggleCartOpen() {
 
 cartIcon.addEventListener("click", toggleCartOpen);
 
-addToCartButton.addEventListener("click", (event) => {
-    const productCard = event.target.closest('.product-card');
-    const productName = productCard.getAttribute("data-name");
-    const price = productCard.getAttribute("data-price");
-    
-    try {
-        cart.addToCart(new Product(productName, price));
-    } catch (error) {
-        alert(error.message);
+
+stock.forEach(product => {
+    const productCard = createProductCard(product);
+    productsList.insertAdjacentHTML("beforeend", productCard);
+})
+
+productsList.addEventListener("click", (event) => {
+    if (event.target.closest("button#add-to-cart")) {
+
+        const productCard = event.target.closest('.product-card');
+        const productName = productCard.getAttribute("data-name");
+        const price = productCard.getAttribute("data-price");
+        
+        try {
+            cart.addToCart(new Product(productName, price));
+            renderCartItems();
+        } catch (error) {
+            alert(error.message);
+        }
     }
 });
+
+
+function createProductCard(productObj) {
+    const { name, price, image} = productObj;
+    return (`
+        <article class="product-card" data-name="${name}" data-price="${price}">
+            <img src="${image}" alt="Apple image">
+            <div class="product-card-info">
+                <h3>Product ${name}</h3>
+                <p>£${price}</p>
+            </div>
+            <div class="product-card-footer">
+                <button id="add-to-cart" >
+                <i class="fas fa-plus-circle"></i> ADD TO CART
+                </button>
+            </div>
+        </article>`
+    )
+}
+
+function renderCartItems() {
+    cartDropdown.innerHTML = "";
+    const cartItems = cart.getCartItems();
+    if (cartItems.length) {
+        const header = `
+            <div class="cart-header">
+                <span>Name</span><span>Qty</span><span>Price</span>
+            </div>
+        `;
+        cartDropdown.insertAdjacentHTML("beforeend", header);
+    } 
+
+    cartItems.forEach(cartItem => {
+        const cartItemNode = `
+            <div class="cart-item">
+                <span>${cartItem.name} x</span>
+                <span>${cartItem.qty}</span>
+                <span>£${(cartItem.price * cartItem.qty).toFixed(2)}</span>
+            </div>
+        `;
+        cartDropdown.insertAdjacentHTML("beforeend", cartItemNode);
+    })
+}
---
+++ stock.js
@@ -0,0 +1,22 @@
+const stock = [
+  {
+    name: "A",
+    price: 0.5,
+    image: "./images/apple.jpg",
+  },
+  {
+    name: "B",
+    price: 0.3,
+    image: "./images/banana.jpg",
+  },
+  {
+    name: "C",
+    price: 0.2,
+    image: "./images/clementine.jpg",
+  },
+  {
+    name: "D",
+    price: 0.15,
+    image: "./images/dragon-fruit.jpg",
+  },
+];
